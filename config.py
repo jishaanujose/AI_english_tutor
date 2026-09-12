ff1463515587264f2270b0c14781362d7913a44e
Jisha Anu Jose <91037105+jishaanujose@users.noreply.github.com>
2026-09-12 14:16:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
+import streamlit as st
 from dotenv import load_dotenv
 
 
@@ -10,7 +10,8 @@ load_dotenv()
 # API
 # =========================================================
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+# 2. Retrieve key from st.secrets (Streamlit Cloud) OR os.getenv (Local .env)
+GROQ_API_KEY = st.secrets.get("GROQ_API_KEY", os.getenv("GROQ_API_KEY"))
 
 
 if not GROQ_API_KEY:
